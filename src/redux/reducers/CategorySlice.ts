9d1ed5d1e45e79e9be2783b459e936f4ddd71f40
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-08-05 14:25:18 +0300

    add category selectors

--- src/redux/reducers/CategorySlice.ts
+++ src/redux/reducers/CategorySlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSelector, createSlice } from "@reduxjs/toolkit";
 import { Category } from "../../models/Category";
+import { RootState } from "../store";
 
 interface InitState {
   items: Category[];
@@ -7,9 +8,9 @@ interface InitState {
 
 const initState: InitState = {
   items: [
-    { id: 1, name: "Task", symbol: "car" },
-    { id: 2, name: "Random Through", symbol: "lightbulb" },
-    { id: 3, name: "Idea", symbol: "idea" },
+    { id: 1, name: "Task", symbol: "cart-fill" },
+    { id: 2, name: "Random Through", symbol: "lightbulb-fill" },
+    { id: 3, name: "Idea", symbol: "lightning" },
   ],
 };
 
@@ -18,5 +19,19 @@ const categorySlice = createSlice({
   initialState: initState,
   reducers: {},
 });
+
+const getCategoriesState = (state: RootState) => state.categories;
+const selectCategoryId = (state: RootState, categoryId: number) => categoryId;
+
+export const selectCategories = createSelector(
+  [getCategoriesState],
+  (state) => state.items
+);
+
+export const selectCategoryById = (categoryId: number) =>
+  createSelector(
+    [getCategoriesState, (state) => selectCategoryId(state, categoryId)],
+    (state, categoryId) => state.items.find((item) => item.id === categoryId)
+  );
 
 export const categoryReducer = categorySlice.reducer;
