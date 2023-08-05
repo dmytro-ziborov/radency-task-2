114921176ed098e7602b1eb9deb8ebe44f1061ee
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-08-05 14:23:49 +0300

    add note selectors

--- src/redux/reducers/NoteSlice.ts
+++ src/redux/reducers/NoteSlice.ts
@@ -79,4 +79,43 @@ const noteSlice = createSlice({
 export const { addNote, updateNote, deleteNote, changeNoteStatus } =
   noteSlice.actions;
 
+//selectors
+const getNoteState = (state: RootState) => state.notes;
+const selectNoteId = (state: RootState, noteId: string) => noteId;
+const selectCategoryId = (state: RootState, categoryId: number) => categoryId;
+const selectNoteStatus = (state: RootState, status: boolean) => status;
+
+//selects notes by status
+const selectNotesByStatus = createSelector(
+  [getNoteState, selectNoteStatus],
+  (notes, status) => notes.items.filter((n) => n.isActive === status)
+);
+
+export const selectActiveNotes = (state: RootState) =>
+  selectNotesByStatus(state, true);
+
+export const selectArchiveNotes = (state: RootState) =>
+  selectNotesByStatus(state, false);
+
+//selects archive notes by category
+export const selectArchivedNotesByCategory = (categoryId: number) =>
+  createSelector(
+    [selectArchiveNotes, (state) => selectCategoryId(state, categoryId)],
+    (notes, categoryId) => notes.filter((note) => note.category === categoryId)
+  );
+
+//select active notes by category
+export const selectActiveNotesByCategory = (categoryId: number) =>
+  createSelector(
+    [selectActiveNotes, (state) => selectCategoryId(state, categoryId)],
+    (notes, categoryId) => notes.filter((note) => note.category === categoryId)
+  );
+
+//select single note by id
+export const selectNoteById = (noteId: string) =>
+  createSelector(
+    [getNoteState, (state) => selectNoteId(state, noteId)],
+    (notes, noteId) => notes.items.find((note) => note.id === noteId)
+  );
+
 export const noteReducer = noteSlice.reducer;
